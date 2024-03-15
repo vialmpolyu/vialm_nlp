223d00755cad5d550c8e3c2c83f7e905965e9b1d
Jason Zhang <155704500+vialmpolyu@users.noreply.github.com>
2024-03-15 15:23:07 +0800

    test task distributer

---
+++ NLP/task_distributer.py
@@ -0,0 +1,22 @@
+from typing import Any, Dict, List
+
+from NLP.vialm_llm import VialmLLM
+
+
+class TaskDistributer():
+    def __init__(
+        self,
+        model: str = "meta-llama/Llama-2-7b-chat-hf" 
+    ) -> None:
+        self._vialm_llm = VialmLLM(model) 
+        with open('NLP/prompt/task_distributer_prompt.txt', 'r') as f:
+            self._base_prompt = f"{''.join(f.readlines())}\n\n"
+
+    def run_analysis(
+            self, 
+            data: List[Dict[str, Any]]
+    ) -> List[str]:
+        prompt = self._vialm_llm.create_prompt(data, self._base_prompt)
+        response = self._vialm_llm.run_inference(prompt)
+
+        return response
